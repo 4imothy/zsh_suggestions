71abd2d7e0932c2284c2357d54194005d3378637
Timothy Cronin <tcroniniv@gmail.com>
2023-02-21 16:14:02 -0500

    merge shell scripts to one

--- src/reader.zsh
+++ src/reader.zsh
@@ -1,6 +1,23 @@
 #!/usr/bin/zsh
 
-text="${READLINE_LINE[1,$READLINE_POINT]}"
+local executables=$(go run main.go)
+# local chip=$(echo $system_hardware | grep "Chip:" | awk '{print($2$3$4)}')
+ 
+__update_command() {
+  zle .$WIDGET
+  local key="${BUFFER: -1}"
+  __CURRENT_INPUT="${__CURRENT_INPUT}${key}"
+}
 
-echo "Currently typed characters: $text"
+__erase_save(){
+  echo "${__CURRENT_INPUT}"
+  __CURRENT_INPUT=""
+}
 
+local __CURRENT_INPUT=""
+
+zle -N self-insert __update_command  
+
+if [[ ! "$precmd_functions" == *__erase_save* ]]; then
+    precmd_functions+=(__erase_save)
+fi 
