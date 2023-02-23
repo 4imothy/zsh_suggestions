2926c550b83412bda17d226416f38a53395a46c2
Timothy Cronin <tcroniniv@gmail.com>
2023-02-23 00:10:11 -0500

    better variables

--- src/reader.zsh
+++ src/reader.zsh
@@ -23,8 +23,8 @@ __remove_char() {
 }
 
 function __find_matches(){
-  __prev_length="${#matches}"
-  matches=()
+  __prev_length="${#__matches}"
+  __matches=()
   # if there are no typed words, then clear the output and return
   if [[ ${#__current_input} -eq 0 ]]; then
     while [ $__prev_length -ge 1 ] 
@@ -34,19 +34,19 @@ function __find_matches(){
     done
     return
   fi 
-  for ex in "${executables[@]}"; do
+  for ex in "${__executables[@]}"; do
     if [[ "$ex" == "$__current_input"* ]]; then
       # if [[ $ex == "source" ]]; then
       #   echo "found $ex"
       # fi
-      if [[ "${#matches}" -lt 5 ]]; then # if the length of matches is less than 5 add
-        matches+=$ex
+      if [[ "${#__matches}" -lt __MAX_LENGTH ]]; then # if the length of matches is less than 5 add
+        __matches+=$ex
       else
         # Find the longest string in the array
         longest_string=""
         index=0
         long_ind=0
-        for string in "${matches[@]}"; do
+        for string in "${__matches[@]}"; do
           index=$((index+1))
           if [[ "${#string}" -gt "${#longest_string}" ]]; then
             longest_string="$string"
@@ -54,17 +54,21 @@ function __find_matches(){
           fi
         done
         if [[ "${#ex}" -lt "${#longest_string}" ]]; then
-              matches[$long_ind]=$ex # replace the longest string 
+              __matches[$long_ind]=$ex # replace the longest string 
         fi
       fi
     fi
   done 
-  # echo -n "\n$matches\033[K" # print out at most the shortest 5 matches, clearing the rest of spaces 
+  # echo -n "\n$matches\033[K" # print out at most the shortest 5 matches, clearing the rest of spaces  
+  unset string
+  unset index
+  unset long_ind
+  unset longest_string
 } 
 
 function __print_matches(){
-  for ((i=1; i<=${#matches[@]}; i+=1)); do 
-    echo -n "\n$matches[$i]\033[K" # print out matches
+  for ((i=1; i<=${#__matches[@]}; i+=1)); do 
+    echo -n "\n$__matches[$i]\033[K" # print out matches
   done 
   # if the matches have less then the previous number in matches then print out blank lines
   remaining=$((__prev_length - i)) 
@@ -73,18 +77,16 @@ function __print_matches(){
     echo -n "\n\033[K"
     ((remaining--))
   done
+  unset remaining
 }
   
 function __set_fut_pos(){ 
   space=$(($(tput lines) - __fut_row))
-  if [[ ${#matches[@]} -ge $space ]]; then # if length is greater than space
-    ((__fut_row-=$((${#matches} - space ))))  # move up length - space
+  if [[ ${#__matches[@]} -ge $space ]]; then # if length is greater than space
+    ((__fut_row-=$((${#__matches} - space ))))  # move up length - space
     ((__fut_row--))
   fi
-  # if [[ __fut_row -ge (($(tput lines) - ${#matches[@]}))  ]]; then
-  #     # subtract an extra when at the bottom the line moves up
-  #     __fut_row=$((__fut_row - ${#matches[@]})) 
-  #   fi
+  unset space
 } 
 
 function __get_cur_pos(){
@@ -93,33 +95,32 @@ function __get_cur_pos(){
   line="${line##*\[}"
   __fut_row=$((${line%%;*}-1)) # extract the row number
   __fut_col=$((${line##*;}-1)) # extract the column number 
+  unset line
 }
 
 __current_input=""
 __fut_row=""
 __fut_col=""
+__MAX_LENGTH=5
+__executables=()
 
 zle -N self-insert __update_command  
 zle -N __remove_in_copy __remove_char # change the action of delete key to deleting most recent and updating __current_input
 bindkey "^?" __remove_in_copy
- 
-# below only happen when file is sourced
-
-local executables=()
 
 # loop through $commands instead of path
 for com in $commands; do
-  executables+=("${com:t}")
+  __executables+=("${com:t}")
 done
 # add all the builtins, cd, source,...
 for cmd in ${(k)builtins}; do 
-  executables+=$cmd
+  __executables+=$cmd
 done 
 # loop through all the aliases and add them to the array
 for alias in ${(k)aliases}; do
-    executables+=("$alias")
+  __executables+=("$alias")
 done 
 # loop through all the functions and add them to the array
 for func in ${(k)functions}; do
-    executables+=("$func")
+  __executables+=("$func")
 done 
