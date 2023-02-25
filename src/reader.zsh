f8f0396f0741444d78b1fdcc34f19ac83fad022d
Timothy Cronin <tcroniniv@gmail.com>
2023-02-25 01:04:51 -0500

    add selection with ctrl+n,p

--- src/reader.zsh
+++ src/reader.zsh
@@ -1,4 +1,5 @@
 #!/usr/bin/zsh
+__MAX_LENGTH=5 # change this to allow for more/less suggestions
  
 function __keypress() {
   zle .$WIDGET
@@ -107,7 +108,11 @@ function __match_execs(){
 
 function __print_matches(){
   for ((i=1; i<=${#__matches[@]}; i+=1)); do 
-    echo -n "\n$__matches[$i]\033[K" # print out matches
+    if [[ $i == $__selected_index ]]; then
+      echo -n "\n\033[45m$__matches[$i]\033[49m\033[K" # print out the selected match with purple background
+    else
+      echo -n "\n$__matches[$i]\033[K" # print out matches
+    fi
   done 
   # if the matches have less then the previous number in matches then print out blank lines
   remaining=$((__prev_length - i)) 
@@ -137,8 +142,50 @@ function __get_cur_pos(){
   unset line
 }
 
-# below only happen when file is sourced at beginning of session
-__MAX_LENGTH=5 # change this to allow for more suggestions
+function __select_next(){
+  if [[ ${#__matches} -eq 0 ]]; then
+    return
+  fi
+
+  __selected_index=$((((((__selected_index)) % ${#__matches})) + 1))
+  __get_cur_pos
+  __print_matches
+  tput cup $__fut_row $__fut_col # add the to_replace - current_input
+  LBUFFER=$__matches[$__selected_index]
+  # future column plus new-prev
+}
+
+function __select_previous(){
+  if [[ ${#__matches} -eq 0 ]]; then
+    return
+  fi
+
+  # 0 -> len 
+  # len -> len - 1
+  # len - 1 -> len - 2
+  # __selected_index=${$(($((((__selected_index % ${#__matches})) - 1))))#-}
+  ((__selected_index--))
+  if [[ $__selected_index -eq -1 || $__selected_index -eq 0 ]]; then
+    __selected_index=${#__matches}
+  fi
+
+  
+  __get_cur_pos
+  __print_matches
+  tput cup $__fut_row $__fut_col # add the to_replace - current_input
+  LBUFFER=$__matches[$__selected_index] 
+}
+
+function reset_selected_index(){
+  __selected_index=0
+}
+
+if [[ ! "$precmd_functions" == *reset_selected_index* ]]; then
+    precmd_functions+=(reset_selected_index)
+fi
+
+
+__selected_index=0
 __current_input=""
 __fut_row=""
 __fut_col=""
@@ -149,11 +196,15 @@ zmodload zsh/mapfile
 __hist=("${(f@)mapfile[$HISTFILE]}") 
 typeset -U __hist # remove duplicates
 __matches=()
-typeset -U __matches
+typeset -U __matches # make it a set
 
 zle -N self-insert __keypress 
 zle -N __remove_in_copy __delete # change the action of delete key to deleting most recent and updating __current_input
+zle -N __next __select_next
+zle -N __prev __select_previous
 bindkey "^?" __remove_in_copy
+bindkey "^P" __prev
+bindkey "^N" __next
 
 # loop through $commands
 for com in $commands; do
