ba47f9a61302615a1e6e4bcc87e7dc17eced17f4
Timothy Cronin <tcroniniv@gmail.com>
2023-03-12 13:26:05 -0400

    use postdisplay

--- src/min_code.zsh
+++ src/min_code.zsh
@@ -3,9 +3,7 @@
 function __keypress() {
   zle .$WIDGET
   __get_cur_pos
-  tput cuf1
-  RBUFFER=""
-  RBUFFER="work"
+  POSTDISPLAY="work"
   tput cup $__fut_row $__fut_col
 }
 
@@ -13,15 +11,12 @@ function __delete() {
   # remove the last character from buffer
   # echo -n "fail\033[K"
   __get_cur_pos
-  RBUFFER=""
   BUFFER=${BUFFER%?}
-  RBUFFER="fuck"
+  region_highlight=("0 10 fg=8")
+  region_highlight=("P0 20 bold memo=foobar")
+  POSTDISPLAY="hello" 
   tput cup $__fut_row $__fut_col
-}
-
-function __clear_rbuffer(){
-  RBUFFER=""
-}
+} 
 
 function __get_cur_pos(){
   echo -ne "\033[6n"
@@ -30,15 +25,11 @@ function __get_cur_pos(){
   __fut_row=$((${line%%;*}-1)) # extract the row number
   __fut_col=$((${line##*;}-1)) # extract the column number 
   unset line
-}
+} 
 
-function clear-rbuffer-and-execute {
-  RBUFFER=""
-  zle accept-line
-}
-
-zle -N clear-rbuffer-and-execute
-bindkey '^M' clear-rbuffer-and-execute 
 zle -N self-insert __keypress 
 zle -N __del __delete # change the action of delete key to deleting most recent and updating __current_input
 bindkey "^?" __del
+region_highlight=("P0 20 bold memo=foobar")
+zle_highlight=(bold)
+region_highlight=(background:red)
