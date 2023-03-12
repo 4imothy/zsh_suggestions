13e59ca78c05f451fdc096214570494f2785ebb0
Timothy Cronin <tcroniniv@gmail.com>
2023-03-12 15:57:47 -0400

    add highlighting

--- src/min_code.zsh
+++ src/min_code.zsh
@@ -2,34 +2,23 @@
 
 function __keypress() {
   zle .$WIDGET
-  __get_cur_pos
-  POSTDISPLAY="work"
-  tput cup $__fut_row $__fut_col
+  POSTDISPLAY="add"
+  __apply_highlighting
 }
 
 function __delete() {
   # remove the last character from buffer
   # echo -n "fail\033[K"
-  __get_cur_pos
   BUFFER=${BUFFER%?}
-  region_highlight=("0 10 fg=8")
-  region_highlight=("P0 20 bold memo=foobar")
-  POSTDISPLAY="hello" 
-  tput cup $__fut_row $__fut_col
-} 
-
-function __get_cur_pos(){
-  echo -ne "\033[6n"
-  read -t 1 -s -d 'R' line < /dev/tty
-  line="${line##*\[}"
-  __fut_row=$((${line%%;*}-1)) # extract the row number
-  __fut_col=$((${line##*;}-1)) # extract the column number 
-  unset line
-} 
+  POSTDISPLAY="del" 
+  __apply_highlighting
+}
 
 zle -N self-insert __keypress 
-zle -N __del __delete # change the action of delete key to deleting most recent and updating __current_input
-bindkey "^?" __del
-region_highlight=("P0 20 bold memo=foobar")
-zle_highlight=(bold)
-region_highlight=(background:red)
+zle -N backward-delete-char __delete
+
+function __apply_highlighting() {
+	if [ $#POSTDISPLAY -gt 0 ]; then
+    region_highlight=("$#BUFFER $(($#BUFFER + $#POSTDISPLAY)) fg=4")
+  fi
+}
